7adf5a142eb8a9ad547206911ce62a725f85ca3a
ruihanzou <stefan.ruihan@gmail.com>
2017-02-09 08:01:36 -0600

    Init commit

---
+++ app/services/statsService.js
@@ -0,0 +1,90 @@
+var geoip = require("geoip-lite");
+var RequestModel = require("../models/requestModel");
+var redis = require("redis");
+var host = process.env.REDIS_PORT_6379_TCP_ADDR;
+var port = process.env.REDIS_PORT_6379_TCP_PORT;
+var redisClient = redis.createClient(port, host);
+
+var logRequest = function (shortUrl, req) {
+    var reqInfo = {};
+    reqInfo.shortUrl = shortUrl;
+    reqInfo.referer = req.headers.referer || "Unknown";
+    reqInfo.platform = req.useragent.platform || "Unknown";
+    reqInfo.browser = req.useragent.browser || "Unknown";
+    var ip = req.headers["x-forwarded-for"] ||
+        req.connection.remoteAddress ||
+        req.socket.remoteAddress ||
+        req.connection.socket.remoteAddress;
+    var geo = geoip.lookup(ip);
+    if (geo) {
+        reqInfo.country = geo.country;
+    } else {
+        reqInfo.country = "Unknown";
+    }
+    reqInfo.timestamp = new Date();
+    var request = new RequestModel(reqInfo);
+    request.save(function (err) {
+        redisClient.publish(shortUrl, shortUrl);
+    });
+};
+
+var getUrlInfo = function (shortUrl, info, callback) {
+    if (info === "totalClicks") {
+        RequestModel.count({ shortUrl: shortUrl }, function (err, data) {
+            callback(data);
+        });
+        return;
+    }
+
+    var groupId = "";
+    if (info === "hour") {
+        groupId = {
+            year: { $year: "$timestamp"},
+            month: { $month: "$timestamp"},
+            day: { $dayOfMonth: "$timestamp"},
+            hour: { $hour: "$timestamp"},
+            minutes: { $minute: "$timestamp"}
+        }
+    } else if (info === "day") {
+        groupId = {
+            year: { $year: "$timestamp"},
+            month: { $month: "$timestamp"},
+            day: { $dayOfMonth: "$timestamp"},
+            hour: { $hour: "$timestamp"}
+        }
+    } else if (info === "month") {
+        groupId = {
+            year: { $year: "$timestamp"},
+            month: { $month: "$timestamp"},
+            day: { $dayOfMonth: "$timestamp"}
+        }
+    } else {
+        groupId = "$" + info;
+    }
+
+    RequestModel.aggregate([
+        {
+            $match: {
+                shortUrl: shortUrl
+            }
+        },
+        {
+            $sort: {
+                timestamp: -1
+            }
+        },
+        {
+            $group: {
+                _id: groupId,
+                count: { $sum: 1 }
+            }
+        }
+    ], function (err, data) {
+        callback(data);
+    });
+};
+
+module.exports = {
+    logRequest: logRequest,
+    getUrlInfo: getUrlInfo
+};
---
+++ app/services/urlService.js
@@ -0,0 +1,109 @@
+var UrlModel = require("../models/urlModel");
+var redis = require("redis");
+var host = process.env.REDIS_PORT_6379_TCP_ADDR;
+var port = process.env.REDIS_PORT_6379_TCP_PORT;
+
+var redisClient = redis.createClient(port, host);
+
+var encode = [];
+var genCharArray = function (charA, charZ) {
+    var arr = [];
+    var i = charA.charCodeAt(0);
+    var j = charZ.charCodeAt(0);
+
+    for (; i <= j; i++) {
+        arr.push(String.fromCharCode(i));
+    }
+    return arr;
+};
+
+encode = encode.concat(genCharArray("A", "Z"));
+encode = encode.concat(genCharArray("a", "z"));
+encode = encode.concat(genCharArray("0", "9"));
+
+var getShortUrl = function (username, longUrl, callback) {
+    if ( longUrl.indexOf("http") === -1) {
+        longUrl = "http://" + longUrl;
+    }
+
+    redisClient.get(username + ":" + longUrl, function (err, shortUrl) {
+        if (shortUrl) {
+            console.log("Byebye mongo!");
+            callback({
+                shortUrl: shortUrl,
+                longUrl: longUrl,
+                username: username
+            });
+        } else {
+            UrlModel.findOne({username: username, longUrl: longUrl}, function (err, data) {
+                if (data) {
+                    callback(data);
+                    redisClient.set(data.shortUrl, data.longUrl);
+                    redisClient.set(username + ":" + data.longUrl, data.shortUrl);
+                } else {
+                    generateShortUrl(function (shortUrl) {
+                        var url = new UrlModel({
+                            shortUrl: shortUrl,
+                            longUrl: longUrl,
+                            username: username,
+                            creationTime: new Date()
+                        });
+                        url.save();
+                        redisClient.set(shortUrl, longUrl);
+                        redisClient.set(username + ":" + longUrl, shortUrl);
+                        callback(url);
+                    });
+                }
+            });
+        }
+    });
+};
+
+var generateShortUrl = function (callback) {
+
+    UrlModel.count({}, function (err, data) {
+        callback(convertTo62(data));
+    });
+
+};
+
+var convertTo62 = function (num) {
+    var result = "";
+    do {
+        result = encode[num % 62] + result;
+        num = Math.floor(num / 62);
+    } while (num);
+
+    return result;
+};
+
+var getLongUrl = function (shortUrl, callback) {
+    redisClient.get(shortUrl, function (err, longUrl) {
+        if (longUrl) {
+            callback({
+                shortUrl: shortUrl,
+                longUrl: longUrl
+            });
+        } else {
+            UrlModel.findOne({shortUrl: shortUrl}, function (err, data) {
+                callback(data);
+                if (data) {
+                    redisClient.set(data.shortUrl, data.longUrl);
+                    redisClient.set(data.longUrl, data.shortUrl);
+                }
+            })
+        }
+    });
+};
+
+var getMyUrls = function (username, callback) {
+    UrlModel.find({username: username}, function (err, urls) {
+        callback(urls);
+    })
+};
+
+module.exports = {
+    getShortUrl: getShortUrl,
+    getLongUrl: getLongUrl,
+    getMyUrls: getMyUrls
+};
